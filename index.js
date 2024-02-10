0e77e4e8a59304f883a58f1757ab3db04be0dbd8
onweb3 <onweb3s@gmail.com>
2024-02-10 23:04:26 +0400

    globally

---
+++ index.js
@@ -0,0 +1,5 @@
+export { default as add } from './src/add.js';
+export { default as subtract } from './src/substract.js';
+export { default as multiply } from './src/multiply.js';
+export { default as divide } from './src/divide.js';
+export { default as percentage } from './src/percentage.js';
